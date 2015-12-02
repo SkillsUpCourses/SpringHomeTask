07f357e86be757ee779843edf06fb27b128b58a1
Ksu <dn100488rol@gmail.com>
2015-12-03 01:07:53 +0200

    Home task with xml

---
+++ NetBeansProjects/com.stoxa_SpringHW_jar_1.0-SNAPSHOT/src/main/java/com/stoxa/springhwannotations/ContactService.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stoxa.springhwannotations;
+
+import java.util.List;
+
+/**
+ *
+ * @author ksu
+ */
+interface ContactService {
+    void addContact (Contact contact);
+    void deleteContact (Contact contact);
+    void updateContact(Contact contact);
+    Contact getContact(String phone);
+    List<Contact> getAllContacts();
+    void clearAll();
+}
